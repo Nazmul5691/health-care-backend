d517b1a7970c538666a4d28372d501e9836c43b7
Fahim Ahammed Firoz <fahimahammed.cse@gmail.com>
2025-10-03 17:32:45 +0600

    v-5: zod schema

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -4,13 +4,15 @@ import { UserService } from "./user.service";
 import sendResponse from "../../shared/sendResponse";
 
 const createPatient = catchAsync(async (req: Request, res: Response) => {
-    const result = await UserService.createPatient(req.body);
+    // const result = await UserService.createPatient(req.body);
+
+    console.log(req.body)
 
     sendResponse(res, {
         statusCode: 201,
         success: true,
         message: "Patient created successfully!",
-        data: result
+        data: ""
     })
 })
 
