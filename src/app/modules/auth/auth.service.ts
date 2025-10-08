adb78b32e6d3203e304c065a42a3223dd4d3dd5d
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-10-09 02:32:12 +0600

    generating-jwt-token

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -2,6 +2,7 @@
 import { prisma } from "../../shared/prisma";
 import { UserStatus } from "@prisma/client";
 import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 
 const login = async (payload: {email: string, password: string}) => {
@@ -18,6 +19,23 @@ const login = async (payload: {email: string, password: string}) => {
     if(!password){
         throw new Error("Password is incorrect")
     }
+
+
+    const accessToken = jwt.sign({email: user.email, role: user.role}, "abcd", {
+        algorithm: "HS256",
+        expiresIn: "1h"
+    })
+    
+    const refreshToken = jwt.sign({email: user.email, role: user.role}, "abcd", {
+        algorithm: "HS256",
+        expiresIn: "1h"
+    })
+
+
+    return {
+        accessToken,
+        refreshToken
+    };
 }
 
 
