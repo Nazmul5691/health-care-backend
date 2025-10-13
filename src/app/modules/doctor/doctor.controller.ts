cff9d896c43172d4e3f50cb4a8e9225eab3266b2
Fahim Ahammed Firoz <fahimahammed.cse@gmail.com>
2025-10-13 22:54:52 +0600

    v7: added

--- src/app/modules/doctor/doctor.controller.ts
+++ src/app/modules/doctor/doctor.controller.ts
@@ -4,10 +4,11 @@ import { IJWTPayload } from "../../types/common";
 import pick from "../../helper/pick";
 import { DoctorService } from "./doctor.service";
 import sendResponse from "../../shared/sendResponse";
+import { doctorFilterableFields } from "./doctor.constant";
 
 const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
     const options = pick(req.query, ["page", "limit", "sortBy", "sortOrder"]);
-    const fillters = pick(req.query, [""])
+    const fillters = pick(req.query, doctorFilterableFields)
 
     const result = await DoctorService.getAllFromDB(fillters, options);
 
@@ -15,7 +16,8 @@ const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
         statusCode: 200,
         success: true,
         message: "Doctor fetched successfully!",
-        data: result
+        meta: result.meta,
+        data: result.data
     })
 })
 
