159f7a190bacd1540e4bd0d809597badc5d92cf6
Fahim Ahammed Firoz <fahimahammed.cse@gmail.com>
2025-10-16 14:57:37 +0600

    updated

--- src/app/modules/appointment/appointment.service.ts
+++ src/app/modules/appointment/appointment.service.ts
@@ -1,6 +1,9 @@
+import { stripe } from "../../helper/stripe";
 import { prisma } from "../../shared/prisma";
 import { IJWTPayload } from "../../types/common";
 import { v4 as uuidv4 } from 'uuid';
+import { IOptions, paginationHelper } from "../../helper/paginationHelper";
+import { Prisma, UserRole } from "@prisma/client";
 
 const createAppointment = async (user: IJWTPayload, payload: { doctorId: string, scheduleId: string }) => {
     const patientData = await prisma.patient.findUniqueOrThrow({
@@ -50,7 +53,7 @@ const createAppointment = async (user: IJWTPayload, payload: { doctorId: string,
 
         const transactionId = uuidv4();
 
-        await tnx.payment.create({
+        const paymentData = await tnx.payment.create({
             data: {
                 appointmentId: appointmentData.id,
                 amount: doctorData.appointmentFee,
@@ -58,13 +61,97 @@ const createAppointment = async (user: IJWTPayload, payload: { doctorId: string,
             }
         })
 
-        return appointmentData;
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ["card"],
+            mode: "payment",
+            customer_email: user.email,
+            line_items: [
+                {
+                    price_data: {
+                        currency: "bdt",
+                        product_data: {
+                            name: `Appointment with ${doctorData.name}`,
+                        },
+                        unit_amount: doctorData.appointmentFee * 100,
+                    },
+                    quantity: 1,
+                },
+            ],
+            metadata: {
+                appointmentId: appointmentData.id,
+                paymentId: paymentData.id
+            },
+            success_url: `https://www.programming-hero.com/`,
+            cancel_url: `https://next.programming-hero.com/`,
+        });
+
+        return { paymentUrl: session.url };
     })
 
 
     return result;
 };
 
+
+const getMyAppointment = async (user: IJWTPayload, filters: any, options: IOptions) => {
+    const { page, limit, skip, sortBy, sortOrder } = paginationHelper.calculatePagination(options);
+    const { ...filterData } = filters;
+
+    const andConditions: Prisma.AppointmentWhereInput[] = [];
+
+    if (user.role === UserRole.PATIENT) {
+        andConditions.push({
+            patient: {
+                email: user.email
+            }
+        })
+    }
+    else if (user.role === UserRole.DOCTOR) {
+        andConditions.push({
+            doctor: {
+                email: user.email
+            }
+        })
+    }
+
+    if (Object.keys(filterData).length > 0) {
+        const filterConditions = Object.keys(filterData).map(key => ({
+            [key]: {
+                equals: (filterData as any)[key]
+            }
+        }))
+
+        andConditions.push(...filterConditions)
+    }
+
+    const whereConditions: Prisma.AppointmentWhereInput = andConditions.length > 0 ? { AND: andConditions } : {};
+
+    const result = await prisma.appointment.findMany({
+        where: whereConditions,
+        skip,
+        take: limit,
+        orderBy: {
+            [sortBy]: sortOrder
+        },
+        include: user.role === UserRole.DOCTOR ?
+            { patient: true } : { doctor: true }
+    });
+
+    const total = await prisma.appointment.count({
+        where: whereConditions
+    });
+
+    return {
+        meta: {
+            total,
+            limit,
+            page
+        },
+        data: result
+    }
+
+}
 export const AppointmentService = {
     createAppointment,
+    getMyAppointment
 };
