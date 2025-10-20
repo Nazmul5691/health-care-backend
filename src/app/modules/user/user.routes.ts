fd55fffe9762c339ba434501ed9725b0e2021c59
Fahim Ahammed Firoz <fahimahammed.cse@gmail.com>
2025-10-20 22:44:50 +0600

    feat(user): add getMyProfile functionality and update routes

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -14,6 +14,12 @@ router.get(
     UserController.getAllFromDB
 )
 
+router.get(
+    '/me',
+    auth(UserRole.ADMIN, UserRole.DOCTOR, UserRole.PATIENT),
+    UserController.getMyProfile
+)
+
 router.post(
     "/create-patient",
     fileUploader.upload.single('file'),
