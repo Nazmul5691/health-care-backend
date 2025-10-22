d87543bcf9d2b01b31aca4638362a1264ee3bc93
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-10-22 23:18:31 +0600

    add payment route

---
+++ src/app/modules/payment/payment.routes.ts
@@ -0,0 +1,8 @@
+// router.post(
+//   "/webhook",
+//   express.raw({ type: "application/json" }), // important for signature verification
+//   stripeWebhookHandler
+// );
+
+
+///////////use route for payment webhook in app.ts file ///////////
