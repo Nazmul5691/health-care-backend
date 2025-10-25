276c9e9a2fee07472a9f1708f14bf7245c1d4ace
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-10-25 22:14:06 +0600

    add nodemailer

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,12 +1,15 @@
-
-import { prisma } from "../../shared/prisma";
-import { UserStatus } from "@prisma/client";
-import bcrypt from 'bcryptjs'
-import { jwtHelpers } from "../../helper/jwtHelpers";
+import { UserStatus } from "@prisma/client"
+import { prisma } from "../../shared/prisma"
+import bcrypt from "bcryptjs";
+import { Secret } from 'jsonwebtoken'
+import ApiError from "../../errors/ApiError";
 import httpStatus from "http-status"
-import ApiError from "../../errors/apiError";
+import config from "../../../config";
+import { emit } from "process";
+import { jwtHelpers } from "../../helper/jwtHelpers";
+import emailSender from "./emailSender";
 
-const login = async (payload: {email: string, password: string}) => {
+const login = async (payload: { email: string, password: string }) => {
     const user = await prisma.user.findUniqueOrThrow({
         where: {
             email: payload.email,
@@ -14,28 +17,175 @@ const login = async (payload: {email: string, password: string}) => {
         }
     })
 
-
-    const password = await bcrypt.compare(payload.password, user.password)
-
-    if(!password){
-        throw new ApiError( httpStatus.BAD_REQUEST,"Password is incorrect")
+    const isCorrectPassword = await bcrypt.compare(payload.password, user.password);
+    if (!isCorrectPassword) {
+        throw new ApiError(httpStatus.BAD_REQUEST, "Password is incorrect!")
     }
 
+    const accessToken = jwtHelpers.generateToken({ email: user.email, role: user.role }, config.jwt.jwt_secret as Secret, "1h");
 
-    const accessToken = jwtHelpers.generateToken({email: user.email, role: user.role}, "abcd", "1h")
-    
-    const refreshToken = jwtHelpers.generateToken({email: user.email, role: user.role}, "abcd", "90d")
-
+    const refreshToken = jwtHelpers.generateToken({ email: user.email, role: user.role }, config.jwt.refresh_token_secret as Secret, "90d");
 
     return {
         accessToken,
         refreshToken,
         needPasswordChange: user.needPasswordChange
-    };
+    }
 }
 
+const refreshToken = async (token: string) => {
+    let decodedData;
+    try {
+        decodedData = jwtHelpers.verifyToken(token, config.jwt.refresh_token_secret as Secret);
+    }
+    catch (err) {
+        throw new Error("You are not authorized!")
+    }
 
+    const userData = await prisma.user.findUniqueOrThrow({
+        where: {
+            email: decodedData.email,
+            status: UserStatus.ACTIVE
+        }
+    });
+
+    const accessToken = jwtHelpers.generateToken({
+        email: userData.email,
+        role: userData.role
+    },
+        config.jwt.jwt_secret as Secret,
+        config.jwt.expires_in as string
+    );
+
+    return {
+        accessToken,
+        needPasswordChange: userData.needPasswordChange
+    };
+
+};
+
+const changePassword = async (user: any, payload: any) => {
+    const userData = await prisma.user.findUniqueOrThrow({
+        where: {
+            email: user.email,
+            status: UserStatus.ACTIVE
+        }
+    });
+
+    const isCorrectPassword: boolean = await bcrypt.compare(payload.oldPassword, userData.password);
+
+    if (!isCorrectPassword) {
+        throw new Error("Password incorrect!")
+    }
+
+    const hashedPassword: string = await bcrypt.hash(payload.newPassword, Number(config.salt_round));
+
+    await prisma.user.update({
+        where: {
+            email: userData.email
+        },
+        data: {
+            password: hashedPassword,
+            needPasswordChange: false
+        }
+    })
+
+    return {
+        message: "Password changed successfully!"
+    }
+};
+
+const forgotPassword = async (payload: { email: string }) => {
+    const userData = await prisma.user.findUniqueOrThrow({
+        where: {
+            email: payload.email,
+            status: UserStatus.ACTIVE
+        }
+    });
+
+    const resetPassToken = jwtHelpers.generateToken(
+        { email: userData.email, role: userData.role },
+        config.jwt.reset_pass_secret as Secret,
+        config.jwt.reset_pass_token_expires_in as string
+    )
+
+    const resetPassLink = config.reset_pass_link + `?userId=${userData.id}&token=${resetPassToken}`
+
+    await emailSender(
+        userData.email,
+        `
+        <div>
+            <p>Dear User,</p>
+            <p>Your password reset link 
+                <a href=${resetPassLink}>
+                    <button>
+                        Reset Password
+                    </button>
+                </a>
+            </p>
+
+        </div>
+        `
+    )
+};
+
+const resetPassword = async (token: string, payload: { id: string, password: string }) => {
+
+    const userData = await prisma.user.findUniqueOrThrow({
+        where: {
+            id: payload.id,
+            status: UserStatus.ACTIVE
+        }
+    });
+
+    const isValidToken = jwtHelpers.verifyToken(token, config.jwt.reset_pass_secret as Secret)
+
+    if (!isValidToken) {
+        throw new ApiError(httpStatus.FORBIDDEN, "Forbidden!")
+    }
+
+    // hash password
+    const password = await bcrypt.hash(payload.password, Number(config.salt_round));
+
+    // update into database
+    await prisma.user.update({
+        where: {
+            id: payload.id
+        },
+        data: {
+            password
+        }
+    })
+};
+
+const getMe = async (session: any) => {
+    const accessToken = session.accessToken;
+    const decodedData = jwtHelpers.verifyToken(accessToken, config.jwt.jwt_secret as Secret);
+
+    const userData = await prisma.user.findUniqueOrThrow({
+        where: {
+            email: decodedData.email,
+            status: UserStatus.ACTIVE
+        }
+    })
+
+    const { id, email, role, needPasswordChange, status } = userData;
+
+    return {
+        id,
+        email,
+        role,
+        needPasswordChange,
+        status
+    }
+
+}
 
 export const AuthService = {
-    login
+    login,
+    changePassword,
+    forgotPassword,
+    refreshToken,
+    resetPassword,
+    getMe
 }
